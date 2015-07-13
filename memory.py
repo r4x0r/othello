c569b062f346060f22ba04eacaca901edf9323a6
Kynaston <kynastonjamison@gmail.com>
2015-07-14 00:32:35 +0800

    updated eval functions

--- memory.py
+++ memory.py
@@ -2,6 +2,6 @@ import os, psutil
 
 def getMemoryUsedMB():
     proc = psutil.Process(os.getpid())
-    return proc.get_memory_info()[0] / float(2**20)
+    return proc.memory_info()[0] / float(2**20)
 
     
